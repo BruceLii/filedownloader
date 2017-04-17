0ca9a328b1d04e45cc6aac610aa740a1a4caed18
liyonglin <liyonglin0704@163.com>
2017-04-17 18:45:04 +0800

    singletask

---
+++ src/DownloadItem.java
@@ -0,0 +1,12 @@
+/**
+ * Created by liyonglin on 2017/4/12.
+ */
+public class DownloadItem {
+    public String img_url;
+    public String saved_file_name;
+
+    public DownloadItem(String img_url, String saved_file_name) {
+        this.img_url = img_url;
+        this.saved_file_name = saved_file_name;
+    }
+}
